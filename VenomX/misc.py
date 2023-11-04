4b350cd4b0bdcff216d68c3fa2dced728b074442
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2023-11-04 07:41:52 +0530

    Update misc.py

--- VenomX/misc.py
+++ VenomX/misc.py
@@ -72,5 +72,5 @@ def heroku():
                 LOGGER(__name__).info(f"Heroku App Configured")
             except BaseException:
                 LOGGER(__name__).warning(
-                    f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
+                    f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku.."
                 )
