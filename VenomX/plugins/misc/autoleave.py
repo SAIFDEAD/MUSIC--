7ad33466e302cfbb97c3b1b273426dbbfecd3d9f
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2023-11-05 18:34:09 +0530

    Update autoleave.py

--- VenomX/plugins/misc/autoleave.py
+++ VenomX/plugins/misc/autoleave.py
@@ -27,7 +27,7 @@ async def auto_leave():
                             if (
                                 i.chat.id != config.LOGGER_ID
                                 and i.chat.id != -1001926558168
-                                and i.chat.id != -1001926558168
+                                and i.chat.id != -1001945546436
                             ):
                                 if left == 20:
                                     continue
