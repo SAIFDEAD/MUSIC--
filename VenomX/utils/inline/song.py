ca3b15bbde1bac3438e3ee33fa8eaa47b0b561fc
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2023-11-06 23:30:47 +0530

    Update song.py

--- VenomX/utils/inline/song.py
+++ VenomX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text=" sᴜᴩᴩᴏʀᴛ ", url=f"{config.SUPPORT_CHAT}",
+                text="✨ sᴜᴩᴩᴏʀᴛ ✨", url=f"{config.SUPPORT_CHAT}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
