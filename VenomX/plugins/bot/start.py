b899ccac8cf5ad9407fde7f1bf8001d035c39607
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2023-12-25 08:25:44 +0530

    Update start.py

--- VenomX/plugins/bot/start.py
+++ VenomX/plugins/bot/start.py
@@ -85,7 +85,7 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         await message.reply_photo(
-            photo=config.START_IMG_URL,
+            photo=random.choice(START_IMG_URL),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
